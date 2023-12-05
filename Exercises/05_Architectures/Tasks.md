8cadf7741ecd830eb38cec6b0c935e080486050b
Fabian Hirter <mail@fabianhirter.ch>
2023-12-05 18:13:40 +0100

    add architecture exercise

---
+++ Exercises/05_Architectures/Tasks.md
@@ -0,0 +1,35 @@
+# Übung Architekturen
+
+Entwickle und implementiere eine Architektur für eine Chat-Applikation.
+
+Halte dein Vorgehen, Überlegungen und Erkenntnisse in einem Dokument fest.
+Schicke dieses am Ende der Unterrichtseinheit dem Dozenten.
+
+Organisiert euch selbstständig in der Klasse, wie ihr die Aufgabe Lösen wollt.
+Stellt sicher, dass alle verstehen, was gemacht wurde.
+
+## Aufgabenstellung
+
+### Architektur
+
+Die Applikation soll Chats zwischen einzelnen Usern sowie in Gruppen ermöglichen.
+Alle Nachrichten sollen in einer Datenbank persistiert werden.
+
+Dazu bietet sich die Publish-Subscribe in Kombination mit einer "Ports & Adaptors" Architektur an.
+
+Entwirf eine Publish-Subscribe Architektur für diese Applikation!
+
+Bestimme ebenfalls das Format der ausgetauschten Nachrichten.
+
+Erstelle dazu ein Dokument in [Markdown](https://www.markdownguide.org/) mit [Mermaid](https://mermaid.js.org/).
+
+### Implementierung
+
+Nutze für die Implementierung dazu den Message-Broker "RabbitMQ" von [CloudAMQP](https://www.cloudamqp.com/).
+
+Erstelle auf CloudAmPQ einen neuen Cluster und verbinde den einfachen [Beispielclient](client.html).
+
+Erweitere die Beispielapplikation:
+
+- Anzeige der verfügbaren User
+- Erstellen von Gruppenchats
---
+++ Exercises/05_Architectures/script.js
@@ -0,0 +1,53 @@
+import {AMQPWebSocketClient} from './amqp-websocket-client.mjs'
+
+const host = "cow-01.rmq2.cloudamqp.com/ws/amqp";
+const vhost = "fggiprgi";
+const username = "fggiprgi";
+const password = "R3dw9XypW-ARjPQUyBwLMDD2tp2x4O3I";
+
+const url = `wss://${host}}`;
+const amqp = new AMQPWebSocketClient(url, `${vhost}`, username, password);
+
+const output = document.getElementById("output");
+const input = document.getElementById("message");
+
+async function start() {
+    try {
+        const connection = await amqp.connect()
+        const channel = await connection.channel()
+        attachPublish(channel)
+        const queue = await channel.queue("")
+        await queue.bind("amq.fanout")
+        const consumer = await queue.subscribe({noAck: false}, (msg) => {
+            console.log("from broker:", msg)
+            output.innerText += msg.bodyToString() + "\n"
+            msg.ack()
+        })
+    } catch (err) {
+        console.error("Error", err, "reconnecting in 1s")
+        disablePublish()
+        setTimeout(start, 1000)
+    }
+}
+
+function attachPublish(ch) {
+    document.forms[0].onsubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await ch.basicPublish("amq.fanout", "", input.value, {contentType: "text/plain"})
+        } catch (err) {
+            console.error("Error", err, "reconnecting in 1s")
+            disablePublish()
+            setTimeout(start, 1000)
+        }
+        input.value = ""
+    }
+}
+
+function disablePublish() {
+    document.forms[0].onsubmit = (e) => {
+        alert("Disconnected, waiting to be reconnected")
+    }
+}
+
+start()
