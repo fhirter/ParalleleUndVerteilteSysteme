4732a43e976f035dcad65f2b11206da03f304e9b
Fabian Hirter <mail@fabianhirter.ch>
2023-12-18 14:53:29 +0100

    update example code

--- Exercises/05_Architectures/script.js
+++ Exercises/05_Architectures/script.js
@@ -1,55 +1,70 @@
-import {AMQPWebSocketClient} from './amqp-websocket-client.mjs'
-
-const host = "cow-01.rmq2.cloudamqp.com/ws/amqp";
-const vhost = "";
-const username = "";
-const password = "";
-
-const AWS_ACCESS_KEY_ID = AKIAIO5FODNN7EXAMPLE;
-
-const url = `wss://${host}}`;
-const amqp = new AMQPWebSocketClient(url, `${vhost}`, username, password);
+import {AMQPWebSocketClient} from "./amqp-websocket-client.mjs";
 
 const output = document.getElementById("output");
 const input = document.getElementById("message");
 
-async function start() {
-    try {
-        const connection = await amqp.connect()
-        const channel = await connection.channel()
-        attachPublish(channel)
-        const queue = await channel.queue("")
-        await queue.bind("amq.fanout")
-        const consumer = await queue.subscribe({noAck: false}, (msg) => {
-            console.log("from broker:", msg)
-            output.innerText += msg.bodyToString() + "\n"
-            msg.ack()
-        })
-    } catch (err) {
-        console.error("Error", err, "reconnecting in 1s")
-        disablePublish()
-        setTimeout(start, 1000)
-    }
-}
-
-function attachPublish(channel) {
+const client = new AmqpClient(() => {
+    // publish messages
     document.forms[0].onsubmit = async (e) => {
         e.preventDefault()
-        try {
-            await channel.basicPublish("amq.fanout", "", input.value, {contentType: "text/plain"})
-        } catch (err) {
-            console.error("Error", err, "reconnecting in 1s")
-            disablePublish()
-            setTimeout(start, 1000)
-        }
+        await client.publish(input.value);
         input.value = ""
     }
-}
 
-function disablePublish() {
-    document.forms[0].onsubmit = (e) => {
-        alert("Disconnected, waiting to be reconnected")
+    // subscribe
+    client.subscribe((message) => {
+        output.innerText += message + "\n";
+    })
+});
+
+
+export function AmqpClient(callback) {
+    const host = "cow-01.rmq2.cloudamqp.com/ws/amqp";
+    const vhost = "fggiprgi";
+    const username = "fggiprgi";
+    const password = "U_tDo0lsDlqKUZgXQ9ywc4K1D3g_j-jo";
+
+    const url = `wss://${host}}`;
+    const amqp = new AMQPWebSocketClient(url, `${vhost}`, username, password);
+
+    let channel;
+    let queue;
+
+    start().then(() => {
+        callback();
+    });
+
+    async function start() {
+        try {
+            const connection = await amqp.connect()
+            channel = await connection.channel()
+            queue = await channel.queue("")
+            await queue.bind("amq.fanout")
+        } catch (err) {
+            console.error("Error", err, "reconnecting in 1s")
+            setTimeout(start, 1000)
+        }
     }
-}
 
-start()
+    async function publish(message) {
+        try {
+            await channel.basicPublish("amq.fanout", "", message, {contentType: "text/plain"})
+        } catch (err) {
+            console.error("Error", err, "reconnecting in 1s")
+            setTimeout(start, 1000)
+        }
+    }
+
+    function subscribe(callback) {
+        queue.subscribe({noAck: false}, (msg) => {
+            console.log("from broker:", msg);
+            callback(msg.bodyToString());
+            msg.ack();
+        })
+    }
+
+    return Object.freeze({
+        publish,
+        subscribe
+    })
+}
