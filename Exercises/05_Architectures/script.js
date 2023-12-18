774d3bd13393305165778ba1fedbfc3c074917bc
Fabian Hirter <mail@fabianhirter.ch>
2023-12-18 14:54:43 +0100

    refactor: move to separate file

--- Exercises/05_Architectures/script.js
+++ Exercises/05_Architectures/script.js
@@ -1,4 +1,5 @@
 import {AMQPWebSocketClient} from "./amqp-websocket-client.mjs";
+import {AmqpClient} from "./amqpClient.mjs";
 
 const output = document.getElementById("output");
 const input = document.getElementById("message");
@@ -18,53 +19,3 @@ const client = new AmqpClient(() => {
 });
 
 
-export function AmqpClient(callback) {
-    const host = "cow-01.rmq2.cloudamqp.com/ws/amqp";
-    const vhost = "fggiprgi";
-    const username = "fggiprgi";
-    const password = "U_tDo0lsDlqKUZgXQ9ywc4K1D3g_j-jo";
-
-    const url = `wss://${host}}`;
-    const amqp = new AMQPWebSocketClient(url, `${vhost}`, username, password);
-
-    let channel;
-    let queue;
-
-    start().then(() => {
-        callback();
-    });
-
-    async function start() {
-        try {
-            const connection = await amqp.connect()
-            channel = await connection.channel()
-            queue = await channel.queue("")
-            await queue.bind("amq.fanout")
-        } catch (err) {
-            console.error("Error", err, "reconnecting in 1s")
-            setTimeout(start, 1000)
-        }
-    }
-
-    async function publish(message) {
-        try {
-            await channel.basicPublish("amq.fanout", "", message, {contentType: "text/plain"})
-        } catch (err) {
-            console.error("Error", err, "reconnecting in 1s")
-            setTimeout(start, 1000)
-        }
-    }
-
-    function subscribe(callback) {
-        queue.subscribe({noAck: false}, (msg) => {
-            console.log("from broker:", msg);
-            callback(msg.bodyToString());
-            msg.ack();
-        })
-    }
-
-    return Object.freeze({
-        publish,
-        subscribe
-    })
-}
