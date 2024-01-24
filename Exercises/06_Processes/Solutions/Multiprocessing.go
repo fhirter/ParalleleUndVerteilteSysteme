140bb2580a8a07768b6ff7211970e23ffdf64420
Fabian Hirter <mail@fabianhirter.ch>
2024-01-24 19:05:25 +0100

    add goroutine solution

--- Exercises/06_Processes/Solutions/Multiprocessing.go
+++ Exercises/06_Processes/Solutions/Multiprocessing.go
@@ -1,59 +1,82 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	const N int = 10000
+	var N = 1000000000
 	const limit int = 1000
 	numbers := numberGenerator(N, limit)
 
+	meanChannel := make(chan int, 1)
+	minChannel := make(chan int, 1)
+	maxChannel := make(chan int, 1)
+
+	log.Printf("N,sequential,concurrent")
+
+	var meanNumber int
+	var minimalNumber int
+	var maximalNumber int
+
 	start := time.Now()
 
-	mean := mean(numbers)
-	minimal := minimal(numbers)
-	maximal := maximal(numbers)
+	mean(numbers, meanChannel)
+	minimal(numbers, minChannel)
+	maximal(numbers, maxChannel)
 
-	log.Printf("main, execution time %s\n", time.Since(start))
+	sequentialTime := time.Since(start)
 
-	fmt.Println(mean)
-	fmt.Println(minimal)
-	fmt.Println(maximal)
+	start = time.Now()
+	go mean(numbers, meanChannel)
+	go minimal(numbers, minChannel)
+	go maximal(numbers, maxChannel)
+
+	meanNumber = <-meanChannel
+	minimalNumber = <-minChannel
+	maximalNumber = <-maxChannel
+
+	concurrentTime := time.Since(start)
+
+	log.Printf("%v, %s, %s", N, sequentialTime, concurrentTime)
+
+	time.Sleep(100 * time.Millisecond)
+
+	log.Println(meanNumber, minimalNumber, maximalNumber)
+
 }
 
-func mean(numbers []int) int {
+func mean(numbers []int, out chan<- int) {
 	if len(numbers) == 0 {
-		return 0
+		out <- 0
 	}
 	var sum int
 	for _, d := range numbers {
 		sum += d
 	}
-	return sum / len(numbers)
+	out <- sum / len(numbers)
 }
 
-func minimal(numbers []int) int {
+func minimal(numbers []int, out chan<- int) {
 	m := 0
 	for i, number := range numbers {
 		if i == 0 || number < m {
 			m = number
 		}
 	}
-	return m
+	out <- m
 }
 
-func maximal(numbers []int) int {
+func maximal(numbers []int, out chan<- int) {
 	m := 0
 	for i, number := range numbers {
 		if i == 0 || number > m {
 			m = number
 		}
 	}
-	return m
+	out <- m
 }
 
 func numberGenerator(N int, limit int) []int {
