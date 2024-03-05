623e351cfb9aebc2ba8ba406791f40c5178e0f66
Fabian Hirter <mail@fabianhirter.ch>
2024-03-05 12:17:46 +0100

    revise exercise

--- Exercises/06_Processes/Tasks.md
+++ Exercises/06_Processes/Tasks.md
@@ -3,6 +3,7 @@
 ## Lernziele
 
 - Die Studierenden wissen, wie Betriebssysteme die Ausführung von mehreren Prozessen ermöglichen.
+- Die Studierenden kennen Kommunikations- und Koordinationstechniken zwischen Prozessen und Systemen
 
 ## Vorgehen
 
@@ -21,7 +22,9 @@ Halte die Erkenntnisse in einem [Markdown-Dokument](https://www.markdownguide.or
 
 **Schicke das Dokument nach dem Unterricht an den Dozenten.**
 
-## Aufgabe
+## Aufgaben
+
+### Systemanalyse
 
 Verbinde dich mit den separat kommunizierten Zugangsdaten mit dem Linux-System.
 
@@ -40,3 +43,46 @@ Lösungsprozess.
 - Wie viele Prozesse starten die einzelnen Applikationen?
 - Welche Prozesse nutzen mehrere Threads?
 
+### Multiprocessing
+
+Entwickle ein Programm, das den Mittelwert einer Liste von Zahlen berechnet.
+
+Das Programm soll eine Liste von zufälligen Zahlen generieren und davon den Mittelwert berechnen.
+
+Verwende für die Lösung [Go](https://go.dev/), dieses bietet sehr einfaches Multiprocessing.
+
+Nutze die [beiliegende Vorlage](Multiprocessing.go) für die Berechnungen.
+
+1. Erstelle eine Statistik, die die gemessenen Zeiten in Abhängigkeit der Länge der Zahlenreihe und der gewählten
+concurrency darstellt.
+2. Bestimme für verschiedene Längen der Zahlenreihe die optimale concurrency.
+3. Recherchiere, wie Go Channels funktionieren und vergleiche mit der Beschreibung im Kapitel 3.6 (Silberschatz, 2019).
+4. Erläutere den Code der Vorlage.
+
+### Scheduling
+
+Löse folgende Aufgabe für verschiedene Scheduling Algorithmen:
+
+- [Priority based, preemptive, round robin](https://en.wikipedia.org/wiki/Fixed-priority_pre-emptive_scheduling)
+- [Completely Fair Scheduler](https://en.wikipedia.org/wiki/Completely_Fair_Scheduler)
+- [Multilevel Feedback Queue](https://de.wikipedia.org/wiki/Multilevel_Feedback_Queue)
+
+| Bezeichner | Priorität | CPU Zeit | Ready Zeit |
+|------------|-----------|----------|------------|
+| P1         | 40        | 20       | 0          |
+| P2         | 30        | 25       | 25         |
+| P3         | 30        | 25       | 30         |
+| P4         | 35        | 15       | 60         |
+| P5         | 5         | 10       | 100        |
+| P6         | 10        | 10       | 105        |
+
+Jeder Prozess hat eine Priorität, wobei eine höhere Zahl eine höhere Priorität bedeutet.
+Zusätzlich zu den aufgeführten Prozessen existiert ein Idle-Task, der ausgeführt wird, wenn keine anderen Prozesse
+verfügbar sind.
+Er besitzt Priorität 0.
+
+Der Scheduler wird alle 10 Zeiteinheiten ausgeführt.
+
+1. Zeichne ein Gantt Diagramm für die Prozessabfolge.
+2. Wie hoch ist die CPU-Auslastung?
+3. Wie gross ist die Wartezeit für jeden Prozess?
--- Exercises/07_Multiprocessing/Solutions/Multiprocessing.go
+++ Exercises/07_Multiprocessing/Solutions/Multiprocessing.go
@@ -7,90 +7,55 @@ import (
 )
 
 func main() {
-
-	//optimizeConcurrent()
-	concurrentVsSequential()
+	log.SetFlags(0)
+	concurrency := 1
+	sampleSize := 10
+	log.Printf("Number of slices: %v", concurrency)
+	for i := 0; i < 8; i++ {
+		calculationTime := concurrentMean(sampleSize, concurrency)
+		log.Printf("%v, %s", sampleSize, calculationTime.Truncate(time.Microsecond).String())
+		sampleSize = sampleSize * 10
+	}
 }
 
-func optimizeConcurrent() {
-	numberOfSlices := 2
+func concurrentMean(sampleSize int, concurrency int) time.Duration {
+	numberOfSlices := concurrency
 
-	log.SetFlags(0)
-	var meanNumber int
-	const limit int = 1000
-	var N = 10
-	//numbers := numberGenerator(N, limit)
-	var channels []chan int
-	for i := 0; i < numberOfSlices; i++ {
-		channels = append(channels, make(chan int, 1))
+	if sampleSize%numberOfSlices > 0 {
+		panic("sampleSize divided by number of slices should not leave a remainder!")
 	}
+
+	const maxNumber int = 1000
+	numbers := numberGenerator(sampleSize, maxNumber)
+
+	resultChannel := make(chan int, numberOfSlices)
+	dataChannel := make(chan []int, numberOfSlices)
 
 	start := time.Now()
+	// fill data channel
 	for i := 0; i < numberOfSlices; i++ {
-		//	go mean(numbers[], channels[i])
+		sliceSize := sampleSize / numberOfSlices
+		dataChannel <- numbers[sliceSize*i : sliceSize*(i+1)]
 	}
+	// run calculations
 	for i := 0; i < numberOfSlices; i++ {
-		_ = <-channels[i]
+		go mean(dataChannel, resultChannel)
 	}
 
-	concurrentTime := time.Since(start)
+	//get results and calculate mean of means
+	var meanNumber = 0
+	for i := 0; i < numberOfSlices; i++ {
+		intermediateMean := <-resultChannel
+		meanNumber += intermediateMean / numberOfSlices
+	}
+	//log.Println(meanNumber)
+	duration := time.Since(start)
 
-	log.Printf("%v, %s", N, concurrentTime.Truncate(time.Millisecond).String())
-	log.Println(meanNumber)
-
+	return duration
 }
 
-func concurrentVsSequential() {
-	log.SetFlags(0)
-
-	var meanNumber int
-	var minimalNumber int
-	var maximalNumber int
-
-	log.Printf("N,sequential,concurrent")
-
-	var N = 10
-	const limit int = 1000
-	for i := 0; i < 10; i++ {
-		numbers := numberGenerator(N, limit)
-
-		meanChannel := make(chan int, 1)
-		minChannel := make(chan int, 1)
-		maxChannel := make(chan int, 1)
-
-		start := time.Now()
-
-		mean(numbers, meanChannel)
-		minimal(numbers, minChannel)
-		maximal(numbers, maxChannel)
-
-		meanNumber = <-meanChannel
-		minimalNumber = <-minChannel
-		maximalNumber = <-maxChannel
-
-		sequentialTime := time.Since(start)
-
-		start = time.Now()
-		go mean(numbers, meanChannel)
-		go minimal(numbers, minChannel)
-		go maximal(numbers, maxChannel)
-
-		meanNumber = <-meanChannel
-		minimalNumber = <-minChannel
-		maximalNumber = <-maxChannel
-
-		concurrentTime := time.Since(start)
-
-		log.Printf("%v, %s, %s", N, sequentialTime.Truncate(time.Microsecond).String(), concurrentTime.Truncate(time.Microsecond).String())
-
-		time.Sleep(100 * time.Millisecond)
-
-		N = N * 10
-	}
-	log.Println(meanNumber, minimalNumber, maximalNumber)
-}
-
-func mean(numbers []int, out chan<- int) {
+func mean(in chan []int, out chan<- int) {
+	numbers := <-in
 	if len(numbers) == 0 {
 		out <- 0
 	}
@@ -99,26 +64,6 @@ func mean(numbers []int, out chan<- int) {
 		sum += d
 	}
 	out <- sum / len(numbers)
-}
-
-func minimal(numbers []int, out chan<- int) {
-	m := 0
-	for i, number := range numbers {
-		if i == 0 || number < m {
-			m = number
-		}
-	}
-	out <- m
-}
-
-func maximal(numbers []int, out chan<- int) {
-	m := 0
-	for i, number := range numbers {
-		if i == 0 || number > m {
-			m = number
-		}
-	}
-	out <- m
 }
 
 func numberGenerator(N int, limit int) []int {
