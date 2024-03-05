6ed4529967444a7943f9b83a9d65a73e2ca35d44
Fabian Hirter <mail@fabianhirter.ch>
2024-03-05 12:33:18 +0100

    improve plotter

--- Exercises/07_Multiprocessing/Solutions/plotter.py
+++ Exercises/07_Multiprocessing/Solutions/plotter.py
@@ -21,16 +21,19 @@ data = pd.read_csv(file_path)
 # Extracting data
 N_values = data['N']
 sequential_times = data['sequential']  # Assuming times are in milliseconds
-concurrent_times = data['concurrent']  # Assuming times are in milliseconds
+concurrent2_times = data['concurrent2']  # Assuming times are in milliseconds
+concurrent5_times = data['concurrent5']  # Assuming times are in milliseconds
 
 # Convert times to seconds for plotting
-sequential_times_s = [parse_time(t) for t in sequential_times]
-concurrent_times_s = [parse_time(t) for t in concurrent_times]
+sequential_times_s = [parse_time(t) / 1000 for t in sequential_times]
+concurrent2_times_s = [parse_time(t) / 1000 for t in concurrent2_times]
+concurrent5_times_s = [parse_time(t) / 1000 for t in concurrent5_times]
 
 # Plotting
 plt.figure(figsize=(10, 6))
 plt.plot(N_values, sequential_times_s, label='Sequential', marker='o')
-plt.plot(N_values, concurrent_times_s, label='Concurrent', marker='o')
+plt.plot(N_values, concurrent2_times_s, label='Concurrent 2', marker='o')
+plt.plot(N_values, concurrent5_times_s, label='Concurrent 5', marker='o')
 
 plt.xscale('log')
 plt.yscale('log')
